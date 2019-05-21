f44f182a07458fd19d09d410b996ca3646a074be
Morgan Villedieu <mvilledieu@magicleap.com>
2019-05-21 15:21:39 -0400

    add controller and refractor webxr class

--- WebXR.js
+++ WebXR.js
@@ -1,193 +1,90 @@
-/**
- * @author mrdoob / http://mrdoob.com
- * @author Mugen87 / https://github.com/Mugen87
- * @modified by mvilledieu / https://github.com/mvilledieu
- */
+class WebXR {
 
-var WEBXR = {
+	constructor(renderer, options) {
+		this.renderer = renderer;
+		this.currentSession = null;
 
-	createButton: function ( renderer, options ) {
-
-		if ( options && options.frameOfReferenceType ) {
-
-			renderer.vr.setFrameOfReferenceType( options.frameOfReferenceType );
-
+		if (options && options.frameOfReferenceType) {
+			this.renderer.vr.setFrameOfReferenceType(options.frameOfReferenceType);
 		}
 
-		function showEnterXR( device ) {
-
-			var currentSession = null;
-
-			function onSessionStarted( session ) {
-
-				session.addEventListener( 'end', onSessionEnded );
-
-				renderer.vr.setSession( session );
-				renderer.vr.enabled = true;
-				button.textContent = 'EXIT XR';
-
-				currentSession = session;
-
-			}
-
-			function onSessionEnded( event ) {
-
-				currentSession.removeEventListener( 'end', onSessionEnded );
-
-				renderer.vr.setSession( null );
-				renderer.vr.enabled = false;
-				button.textContent = 'ENTER XR';
-
-				currentSession = null;
-
-			}
-
-			button.style.display = '';
-
-			button.style.cursor = 'pointer';
-			button.style.left = 'calc(50% - 50px)';
-			button.style.width = '100px';
-
-			button.textContent = 'ENTER XR';
-
-			button.onmouseenter = function () { button.style.opacity = '1.0'; };
-			button.onmouseleave = function () { button.style.opacity = '0.5'; };
-
-			button.onclick = function () {
-
-				if ( currentSession === null ) {
-
-					navigator.xr.requestSession( { mode: 'immersive-ar' } ).then( onSessionStarted );
-
-				} else {
-
-					currentSession.end();
-
-				}
-
-			};
-
-			if ( device ) {
-
-				renderer.vr.setDevice( device );
-
-			}
-
-		}
-
-		function showVRNotFound() {
-
-			button.style.display = '';
-
-			button.style.cursor = 'auto';
-			button.style.left = 'calc(50% - 75px)';
-			button.style.width = '150px';
-
-			button.textContent = 'XR/VR NOT FOUND';
-
-			button.onmouseenter = null;
-			button.onmouseleave = null;
-
-			button.onclick = null;
-
-			renderer.vr.setDevice( null );
-
-		}
-
-		function stylizeElement( element ) {
-
-			element.style.position = 'absolute';
-			element.style.bottom = '20px';
-			element.style.padding = '12px 6px';
-			element.style.border = '1px solid #fff';
-			element.style.borderRadius = '4px';
-			element.style.background = 'rgba(0,0,0,0.1)';
-			element.style.color = '#fff';
-			element.style.font = 'normal 13px sans-serif';
-			element.style.textAlign = 'center';
-			element.style.opacity = '0.5';
-			element.style.outline = 'none';
-			element.style.zIndex = '999';
-
-		}
-
-		if ( 'xr' in navigator ) {
-
-			var button = document.createElement( 'button' );
-			button.style.display = 'none';
-
-			stylizeElement( button );
-
-			navigator.xr.supportsSessionMode( 'immersive-ar' ).then( function () {
-				showEnterXR( );
-			});
-
-			return button;
-
-		} else if ( 'getVRDisplays' in navigator ) {
-
-			var button = document.createElement( 'button' );
-			button.style.display = 'none';
-
-			stylizeElement( button );
-
-			window.addEventListener( 'vrdisplayconnect', function ( event ) {
-
-				showEnterVR( event.display );
-
-			}, false );
-
-			window.addEventListener( 'vrdisplaydisconnect', function ( event ) {
-
-				showVRNotFound();
-
-			}, false );
-
-			window.addEventListener( 'vrdisplaypresentchange', function ( event ) {
-
-				button.textContent = event.display.isPresenting ? 'EXIT VR' : 'ENTER VR';
-
-			}, false );
-
-			window.addEventListener( 'vrdisplayactivate', function ( event ) {
-
-				event.display.requestPresent( [ { source: renderer.domElement } ] );
-
-			}, false );
-
-			navigator.getVRDisplays()
-				.then( function ( displays ) {
-
-					if ( displays.length > 0 ) {
-
-						showEnterVR( displays[ 0 ] );
-
-					} else {
-
-						showVRNotFound();
-
-					}
-
-				} ).catch( showVRNotFound );
-
-			return button;
-
+		this.button = this.createButton();
+
+		if ('xr' in navigator && 'supportsSessionMode' in navigator.xr) {
+			navigator.xr.supportsSessionMode('immersive-ar') //MagicLeap
+				.then(this.showEnterXR)
+				.catch(this.showXRNotFound);
 		} else {
-
-			var message = document.createElement( 'a' );
-			message.href = 'https://webvr.info';
-			message.innerHTML = 'WEBVR/WEBXR NOT SUPPORTED';
-
-			message.style.left = 'calc(50% - 90px)';
-			message.style.width = '180px';
-			message.style.textDecoration = 'none';
-
-			stylizeElement( message );
-
-			return message;
-
+			this.showXRNotFound();
 		}
-
 	}
 
-};
+	onSessionStarted = session => {
+		session.addEventListener('end', this.onSessionEnded);
+
+		this.renderer.vr.setSession(session);
+		this.renderer.vr.enabled = true;
+		this.button.textContent = 'EXIT WEBXR';
+
+		this.currentSession = session;
+	}
+
+	onSessionEnded = () => {
+		this.currentSession.removeEventListener('end', this.onSessionEnded);
+
+		this.renderer.vr.setSession(null);
+		this.renderer.vr.enabled = false;
+		this.button.textContent = 'ENTER WEBXR';
+
+		this.currentSession = null;
+	}
+
+	showEnterXR = device => {
+		if (device) this.renderer.vr.setDevice(device);
+		this.button.textContent = 'ENTER WEBXR';
+		this.button.onclick = () => {
+			if (this.currentSession === null) {
+				navigator.xr.requestSession({
+						mode: 'immersive-ar'
+					})
+					.then(this.onSessionStarted);
+			} else {
+				this.currentSession.end();
+			}
+		};
+	}
+
+	showXRNotFound = () => {
+		this.button.textContent = 'WEBXR NOT FOUND';
+		this.renderer.vr.setDevice(null);
+	}
+
+	createButton() {
+		const button = document.createElement('button');
+
+		button.textContent = `ENTER`;
+		button.style.position = 'absolute';
+		button.style.bottom = '20px';
+		button.style.padding = '12px 6px';
+		button.style.border = '1px solid #fff';
+		button.style.borderRadius = '4px';
+		button.style.background = 'rgba(0,0,0,0.1)';
+		button.style.color = '#fff';
+		button.style.font = 'normal 13px sans-serif';
+		button.style.textAlign = 'center';
+		button.style.opacity = '0.5';
+		button.style.outline = 'none';
+		button.style.zIndex = '999';
+		button.style.transform = 'translate(-50%, -50%)';
+		button.style.left = '50%';
+		button.style.cursor = 'pointer';
+
+		button.onmouseenter = () => button.style.opacity = '1.0';
+		button.onmouseleave = () => button.style.opacity = '0.5';
+
+		return button;
+	}
+
+	getButton = () => this.button;
+	
+}
